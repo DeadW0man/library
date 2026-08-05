759f4505b4804f6df05f07ab06d935d6bdf05267
DeadW0man <vpod1205@gmail.com>
2026-08-05 15:22:35 +0300

    Update http.py

--- app/http/http.py
+++ app/http/http.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from pydantic import ValidationError
-from app.utils.auth import is_auth_valid, get_current_user
 from app.schemas.schemas import UserRegister, UserLogin, BookCreate, ShareCreate, ShareReturn
 import app.services.services as service
 
