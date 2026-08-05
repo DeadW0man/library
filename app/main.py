39d54db60d6e3e54f5e23c8018ae2e4d1f4ce8bc
DeadW0man <vpod1205@gmail.com>
2026-08-05 15:23:22 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from db.database import prepare_tables
 solaris_app = Flask('solaris')
 
 def run_app() -> None:
-    # Просто запускаем наше приложение
     solaris_app.run(
         host=getenv('SOLARIS_HOST', '0.0.0.0'),
         port=int(getenv('SOLARIS_PORT', '5000')),
